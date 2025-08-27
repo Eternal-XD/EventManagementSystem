c0b3c04a9de60072da7f57ba0f451f1a15c01a26
Eternal-XD <tharudeneth621@gmail.com>
2025-08-28 05:00:18 +0530

    Add files via upload

---
+++ EventManagementSystem/DbConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace EventManagementSystem   // make sure spelling matches your project
+{
+    public class DbConnection
+    {
+        // connection string to your database
+        private readonly string connectionString = "server=localhost;uid=root;pwd=;port=3306;database=ems;";
+
+        // THIS method is what your controller is trying to call
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
