8bc8453a43cf9d93eeb280d6782d237082052f05
Eternal-XD <tharudeneth621@gmail.com>
2025-09-07 03:11:08 +0530

    changing directories
    accidently uploaded the model file into the model file causing these files to be in EventManagementSystem/Model/Model

---
+++ Model/Ticket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagmentSystem.Model
+{
+    class Ticket
+    {
+        public int Id { get; set; }
+        public Events Event { get; set; }
+
+        public string TicketType { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public bool Available { get; set; }
+
+        public Ticket(Events eventItem, string ticketType, double price, int quantity)
+        {
+            Event = eventItem;
+            TicketType = ticketType;
+            Price = price;
+            Quantity = quantity;
+            Available = true;
+        }
+
+    }
+}
---
+++ Model/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagmentSystem.Model
+{
+    public enum UserRole
+    {
+        Attendee,
+        Organizer,
+        Admin
+    }
+}
+
